8f83837394612684c7e274947baf2c5b7fb6271b
Sivaramakrishnan Swaminathan <sivaramakrishnan@utexas.edu>
2016-09-06 11:54:40 -0400

    small improvements

--- testOptimization.jl
+++ testOptimization.jl
@@ -13,9 +13,9 @@ isingH, Dmax = build_H_Ising();
 # TRAINING HYPER-PARAMETERS
 #----------------------------------------------------------------------------
 
-parameters_init = Dict(:energyDelta => 1e-10 , :Qsweep => 1000 , :Qlayer => 4, :Qsingle => 3);
-parameters_graft = Dict(:energyDelta => 1e-10, :Qsweep => 600, :Qlayer => 4, :Qsingle => 3);
-parameters_fullsweep = Dict(:energyDelta => 1e-10 , :Qsweep => 2000 , :Qlayer => 4, :Qsingle => 3);
+parameters_init = Dict(:energyDelta => 1e-10 , :Qsweep => 201 , :Qlayer => 5, :Qsingle => 4);
+parameters_graft = Dict(:energyDelta => 1e-10, :Qsweep => 601, :Qlayer => 4, :Qsingle => 3);
+parameters_sweep = Dict(:energyDelta => 1e-10 , :Qsweep => 1001 , :Qlayer => 4, :Qsingle => 3);
 
 const LAYER_SHAPE=(8,5,5,5,5,5,5,5,5,5)
 const INIT_LAYERS=7
@@ -25,7 +25,7 @@ const INIT_LAYER_SHAPE=LAYER_SHAPE[1:(INIT_LAYERS+1)]
 println("Shape of init layers -- ", INIT_LAYER_SHAPE)
 println("Initial optimization -- ", parameters_init)
 println("Layer optimization -- ", parameters_graft)
-println("Sweep optimization -- ", parameters_fullsweep)
+println("Sweep optimization -- ", parameters_sweep)
 println(string(map((x) -> '-', collect(1:28))...))
 println()
 
@@ -33,13 +33,13 @@ println()
 # PRE-TRAINING
 #----------------------------------------------------------------------------
 
-## Load already optimized 6-layer MERA
+## Load already optimized 7-layer MERA
 m = load("solutionMERA_7layers_(8,5,5,5,5,5,5,5)shape.jld","m_7layers")
 
 # m = generate_random_MERA(INIT_LAYER_SHAPE);
 # println("Starting the optimization...")
 
-improveGraft!(isingH, m, parameters_init, 2)
+improveGraft!(isingH, m, parameters_init,1)
 save("solutionMERA_$(INIT_LAYERS)layers_$(INIT_LAYER_SHAPE)shape.jld", "m_$(INIT_LAYERS)layers", m)
 println(string(map((x) -> '-', collect(1:28))...))
 
@@ -51,10 +51,15 @@ println(string(map((x) -> '-', collect(1:28))...))
 for lyr in (INIT_LAYERS+1):(length(LAYER_SHAPE)-1)
     println("\nNow adding layer number: ", lyr)
     exact_persite = exact_energy_persite(lyr);
-    println("Exact per-site energy for this depth: ", exact_persite,"\n")
+    println("Not always exact per-site energy for this depth: ", exact_persite,"\n")
 
     newLayer = generate_random_layer(LAYER_SHAPE[lyr],LAYER_SHAPE[lyr+1])
     push!(m.levelTensors, newLayer)
+    # It is tempting to guess a better initialization for the new layer.
+    # Ideally, it is tempting to use the penultimate layer,
+    # since they must be the same at the critical point, etc...
+    # but local "gauge" freedom will probably make that quite useless
+
     # Improving the newly added layer and the top tensor
     energy_persite = improveGraft!(isingH, m, parameters_graft, 1)
     # It's important to iterate over the top layer several times,
@@ -65,10 +70,11 @@ for lyr in (INIT_LAYERS+1):(length(LAYER_SHAPE)-1)
     energy_persite = improveGraft!(isingH, m, parameters_sweep, 4)
 
     # sweep over all layers
-    energy_persite = improveGraft!(isingH, m, parameters_fullsweep)
+    energy_persite = improveGraft!(isingH, m, parameters_sweep)
 
     println("\nFinal energy of this optimized MERA: ", energy_persite)
-    println("Off from the 1/Nsq corrected answer by: ", (energy_persite - exact_persite)/(exact_persite) )
+    println("Not always exact per-site energy for this depth: ", exact_persite,"\n")
+    println("Fractional error in our variational estimate: ", (energy_persite - exact_persite)/(exact_persite) )
 
     save("solutionMERA_$(lyr)layers_$(LAYER_SHAPE[1:lyr+1])shape.jld", "m_$(lyr)layers", m)
     println(string(map((x) -> '-', collect(1:28))...))

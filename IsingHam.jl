6c4a7eef6623116b8394cf835412349383042598
Sivaramakrishnan Swaminathan <sivaramakrishnan@utexas.edu>
2016-09-06 11:49:10 -0400

    Augmenting list of exact energies with approximate estimates
    when number of sites becomes too large to diagonalize exactly

--- IsingHam.jl
+++ IsingHam.jl
@@ -26,14 +26,21 @@ function build_H_Ising(h=1.0)
     return h, D_max
 end
 
+function approximate_energy_persite_PBC(nsites)
+    # including only the leading finite-size correction
+    return ( -4/pi - (pi/6)/(nsites*nsites) )
+end
+
 function exact_energy_persite(n_lyr)
     # Obtained by exact diagonalization of a free fermions system with the opposite BCs
     EnAPBC=[-1.270005811417927, -1.2724314193572888, -1.2730375326245706, -1.273189042909428, -1.2732269193538452, -1.2732363883945284];
-    EnPBC=[-1.2797267740319183, -1.2748570272966502, -1.273643645891852, -1.273340553194287, -1.2732647957982595, -1.273245857435202, -1.273241122906045, -1.273239939277603];
-    return EnPBC[n_lyr]
-end
 
-function approximate_energy_persite(nsites)
-    # including only the leading finite-size correction
-    return ( -4/pi - (pi/6)/(nsites*nsites) )
+    EnPBC_exactdiag_1_8=[-1.2797267740319183, -1.2748570272966502, -1.273643645891852, -1.273340553194287, -1.2732647957982595, -1.273245857435202, -1.273241122906045, -1.273239939277603];
+
+    EnPBC_approx_9_15 = map(approximate_energy_persite_PBC,81*4.^collect(9:15));
+
+    EnPBC=[EnPBC_exact_diag...,EnPBC_approx_9_15...]
+    # Exact results only for up to 8 layers. Beyond that, 1/Nsq approximation is good enough since our MERA is not yet that accurate
+
+    return EnPBC[n_lyr]
 end

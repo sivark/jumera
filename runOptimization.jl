b1e427308606ec86838312deeb5852fa88ef0b37
Sivaramakrishnan Swaminathan <sivaramakrishnan@utexas.edu>
2016-10-05 12:47:53 -0400

    Imports suitable parameters and outputs them first

--- runOptimization.jl
+++ runOptimization.jl
@@ -1,19 +1,28 @@
 typealias Float Float64
 @show(Float)
 
-using JLD
+include("trainingParams.jl")
+
+# Print out the hyperparameters
+println("Shape of init layers -- ", INIT_LAYER_SHAPE)
+println("Init  optimization -- ", parameters_init )
+println("Graft optimization -- ", parameters_graft)
+println("Sweep optimization -- ", parameters_sweep)
+println(string(map((x) -> '-', collect(1:28))...))
+println()
 
 #----------------------------------------------------------------------------
 # Including MERA code
 #----------------------------------------------------------------------------
 
-include("trainingParams.jl")
 include("IsingHam.jl")
 include("BinaryMERA.jl")
 include("OptimizeMERA.jl")
 
 println("Going to build the Ising micro hamiltonian.")
 isingH, Dmax = build_H_Ising();
+
+using JLD
 
 #----------------------------------------------------------------------------
 # PRE-TRAINING

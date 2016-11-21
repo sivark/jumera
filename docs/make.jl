8ed5144b49f7a1635424e22fab551621011ca63e
Sivaramakrishnan Swaminathan <sivaramakrishnan@utexas.edu>
2016-11-21 01:20:17 -0500

    Setting up to use the template feature in DocStringsExtensions.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,24 @@
 push!(LOAD_PATH,"../")
 using Documenter
+using DocStringExtensions
+
+@template DEFAULT =
+    """
+    $(DOCSTRING)
+    """
+
+@template TYPES =
+    """
+    $(TYPEDEF)
+    $(DOCSTRING)
+    """
+
+@template (METHODS, MACROS) =
+    """
+    $(SIGNATURES)
+    $(DOCSTRING)
+    $(METHODLIST)
+    """
+
 
 makedocs()
